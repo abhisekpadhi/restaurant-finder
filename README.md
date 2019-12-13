1159922061e9c3f9ad3bef2101282fab0faa592a
abhisekpadhi <abhisek.p@udaan.com>
2019-12-14 03:13:52 +0530

    Intial commit

---
+++ README.md
@@ -0,0 +1,141 @@
+# Restaurant Finder
+Tiny tool to help you find a restaurant in an instant.
+
+# How to run it
+1. Clone the repo
+```
+```
+
+2. Setup virtualenv
+```
+cd restaurant-finder
+virtualenv venv
+source venv/bin/activate
+```
+
+3. Install dependencies
+```
+pip install -r requirements.txt
+```
+
+4. Run the tool
+```
+➜  ppt python restaurant_finder.py
+What is your location?
+BTM Stage 1
+-> You are in Bengaluru
+Here are the cuisines in Bengaluru, select your preference:
+1035: Afghan
+152: African
+1: American
+2: Andhra
+4: Arabian
+3: Asian
+165: Assamese
+131: Australian
+292: Awadhi
+193: BBQ
+5: Bakery
+227: Bar Food
+132: Belgian
+10: Bengali
+270: Beverages
+1013: Bihari
+7: Biryani
+1032: Bohri
+247: Bubble Tea
+168: Burger
+22: Burmese
+30: Cafe
+121: Cantonese
+994: Charcoal Chicken
+18: Chettinad
+25: Chinese
+1040: Coffee
+35: Continental
+1014: Cuisine Varies
+100: Desserts
+268: Drinks Only
+38: European
+40: Fast Food
+271: Finger Food
+45: French
+501: Frozen Yogurt
+134: German
+47: Goan
+156: Greek
+48: Gujarati
+143: Healthy Food
+1026: Hot dogs
+49: Hyderabadi
+233: Ice Cream
+114: Indonesian
+140: Iranian
+55: Italian
+60: Japanese
+164: Juices
+65: Kashmiri
+178: Kebab
+62: Kerala
+63: Konkan
+67: Korean
+66: Lebanese
+157: Lucknowi
+102: Maharashtrian
+69: Malaysian
+71: Malwani
+72: Mangalorean
+70: Mediterranean
+73: Mexican
+137: Middle Eastern
+1041: Mishti
+1015: Mithai
+1018: Modern Indian
+1051: Momos
+74: Mongolian
+147: Moroccan
+75: Mughlai
+166: Naga
+117: Nepalese
+231: North Eastern
+50: North Indian
+1057: Odia
+1048: Paan
+290: Parsi
+162: Peruvian
+82: Pizza
+87: Portuguese
+88: Rajasthani
+27: Raw Meats
+1005: Roast Chicken
+1023: Rolls
+84: Russian
+998: Salad
+304: Sandwich
+83: Seafood
+993: Sindhi
+119: Singaporean
+972: South American
+85: South Indian
+89: Spanish
+86: Sri Lankan
+141: Steak
+90: Street Food
+177: Sushi
+1054: Tamil
+163: Tea
+150: Tex-Mex
+95: Thai
+93: Tibetan
+142: Turkish
+99: Vietnamese
+1024: Wraps
+What cusines are in you mood for?
+83 119             
+Here is the best restaurant we found for you:
+
+Name: Sriracha Robata & Contemporary Pan Asian Dining
+Find on Zomato: https://www.zomato.com/bangalore/sriracha-robata-contemporary-pan-asian-dining-indiranagar-bangalore?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1
+Address: 608, 12th Main Road, HAL, 2nd Stage, Indiranagar, Bangalore
+Find on Maps: https://maps.google.com/?q=608+12th+Main+Road+HAL+2nd+Stage+Indiranagar+Bangalore
+```
---
+++ restaurant_finder.py
@@ -0,0 +1,77 @@
+import os
+import requests
+
+MAPS_API_KEY = os.environ['MAPS_API_KEY']
+ZOMATO_API_KEY = os.environ['ZOMATO_API_KEY']
+headers = {"user-key": ZOMATO_API_KEY, "Accept": "application/json"}
+
+# Test data
+# lat, lng = 12.9199409, 77.61065119999999
+# city_id = 4
+# cuisine_id = 1024
+
+def find_geocode_by_name(name):
+    """
+    Returns geocode (lat, lng) from location name
+    """
+    address = name.replace(' ', '+')
+    url = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={MAPS_API_KEY}'
+    response = requests.get(url)
+    lat, lng = response.json()['results'][0]['geometry']['location']['lat'], response.json()['results'][0]['geometry']['location']['lng']
+    return (lat, lng)
+
+
+def get_city_by_geocode(lat, lng):
+    """
+    Returns (city_id, city_name) for geocode (lat, lng)
+    """
+    url = f'https://developers.zomato.com/api/v2.1/cities?lat={lat}&lon={lng}'
+    response = requests.get(url, headers=headers)
+    return (response.json()['location_suggestions'][0]['id'], response.json()['location_suggestions'][0]['name'])
+
+
+def get_cusines_by_city(city_id):
+    """
+    Returns cuisines for city_id
+    """
+    url = f'https://developers.zomato.com/api/v2.1/cuisines?city_id={city_id}'
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def search_restaurant(city_id, cuisine_ids):
+    """
+    Returns restaurants in a city who server cuisine
+    """
+    cuisine_ids_comma_separated = ','.join(cuisine_ids)
+    url = f'https://developers.zomato.com/api/v2.1/search?entity_id={city_id}&entity_type=city&cuisines={cuisine_ids_comma_separated}'
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def get_best_restaurants(search_results):
+    best_restaurants = []
+    for restaurant in search_results['restaurants']:
+        if float(restaurant['restaurant']['user_rating']['aggregate_rating']) > 4.5:
+            best_restaurants.append(restaurant)
+    return best_restaurants
+
+def get_google_maps_url(address):
+    cleaned_address = address.replace(',', '').replace(' ', '+')
+    return f'https://maps.google.com/?q={cleaned_address}'
+
+if __name__ == '__main__':
+    location = input("What is your location? \n")
+    lat, lng = find_geocode_by_name(location)
+    city_id, city_name = get_city_by_geocode(lat, lng)
+    print(f'-> You are in {city_name}')
+    print(f'Here are the cuisines in {city_name}, select your preference:')
+    cuisines = get_cusines_by_city(city_id)['cuisines']
+    for cuisine in cuisines:
+        print(f'{cuisine["cuisine"]["cuisine_id"]}: {cuisine["cuisine"]["cuisine_name"]}')
+    cuisine_ids = input("What cusines are in you mood for? \n").split(' ')
+    print('Here is the best restaurant we found for you:\n')
+    search_results = search_restaurant(city_id, cuisine_ids)
+    best_restaurants = get_best_restaurants(search_results)
+    print('Name: ' + best_restaurants[0]['restaurant']['name'])
+    print('Find on Zomato: ' + best_restaurants[0]['restaurant']['url'])
+    print('Address: ' + best_restaurants[0]['restaurant']['location']['address'])
+    print('Find on Maps: ' + get_google_maps_url(best_restaurants[0]['restaurant']['location']['address']))
